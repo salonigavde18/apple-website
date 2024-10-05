eb18133e2b8bf8b2bd36b8db1f85ee77c7fca9be
Saloni Gavde <saloniigavde2002@gmail.com>
2024-10-05 20:44:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
+import Model from './components/Model'
+import * as Sentry from '@sentry/react'
+import Features from './components/Features'
+
+function App() {
+  
+  return (
+    <main className='bg-black'>
+      <Navbar/> 
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React, { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://628f7aa0336a3be7b761b194b90cfa3a@o4508070437584896.ingest.us.sentry.io/4508070439616512",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
